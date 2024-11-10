72e850fee19a3dd3ab60bc8bd2eddb714b88464f
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2024-11-10 23:11:35 +0100

    Styling Logout button

--- app/screen/Dashboard.tsx
+++ app/screen/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { DarkTheme, NavigationProp } from '@react-navigation/native';
 import { FIREBASE_AUTH } from '../../FirebaseConfig';
@@ -11,7 +11,9 @@ const Dashboard = ( {navigation}: RouterProps ) => {
   return (
     <View style={styles.view}>
         <Text>Tutaj beda dane takie jak zarobki i inne statystyki w formie wykresow</Text>
-        <Button onPress={() => FIREBASE_AUTH.signOut()} title='Logout'></Button>
+          <Pressable style={styles.button} onPress={() => FIREBASE_AUTH.signOut()}>
+            <Text style={styles.text}>Logout</Text>
+          </Pressable>
     </View>
   )
 }
@@ -23,5 +25,23 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
+    button: {
+      marginVertical: 4,
+      marginHorizontal: 120,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 30,
+      borderRadius: 4,
+      elevation: 3,
+      backgroundColor: 'black',
+    },
+    text: {
+      fontSize: 16,
+      lineHeight: 21,
+      fontWeight: 'bold',
+      letterSpacing: 0.25,
+      color: 'white',
+    },
 })
