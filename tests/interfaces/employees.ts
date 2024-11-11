317bdf31aacd7238e09f7ad7da4dad87384b3c51
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2024-11-11 14:39:04 +0100

    Added interface

---
+++ tests/interfaces/employees.ts
@@ -0,0 +1,11 @@
+interface Employees {
+    Login: Array<string>,
+    Password: string,
+}
+
+const employees: Employees ={
+    Login: [ "testemp0@test.com", "testemp1@test.com", "testemp2@test.com", "testemp3@test.com"],
+    Password: "test123"
+}
+
+export default employees;
