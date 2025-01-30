544ffa086d0c1ab454601cc600c3a94ccba30a62
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-01-30 22:32:13 +0100

    New interface for menager

---
+++ tests/interfaces/menager.ts
@@ -0,0 +1,11 @@
+interface Employees {
+    Login: Array<string>,
+    Password: string,
+}
+
+const employees: Employees ={
+    Login: [ "testmena@test.com"],
+    Password: "test123",
+}
+
+export default employees;
